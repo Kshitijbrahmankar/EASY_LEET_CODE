178e291afaf2930a95caee8a7184a7e3e2ff18f8
Kshitijbrahmankar <153193909+Kshitijbrahmankar@users.noreply.github.com>
2026-06-08 01:51:57 +0530

    Add files via upload

---
+++ 70. Climbing Stairs.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 2:
+            return n
+        
+        prev1 = 2
+        prev2 = 1
+
+        for _ in range(3, n + 1):
+            curr = prev1 + prev2
+            prev2 = prev1
+            prev1 = curr
+        return prev1
